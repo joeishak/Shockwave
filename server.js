6fb04a919551fd6667f93adce3a3af660c21831d
jrcamilon <ecamilon@infosol.com>
2020-01-15 10:22:10 -0700

    Change port to 3094

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function(err,req,res,next) {
 router(app);
 
 //Server Setup
-var port = process.env.PORT || 3090;
+var port = process.env.PORT || 3094;
 let server = http.createServer(app);
 server.listen(port);
 
